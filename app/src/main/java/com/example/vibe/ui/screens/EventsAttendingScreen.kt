15f6f13fae38a0ff4b867c70f25243461e48d6fa
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2025-02-22 22:43:43 -0500

    building 'events attending' page

--- app/src/main/java/com/example/vibe/ui/screens/EventsAttendingScreen.kt
+++ app/src/main/java/com/example/vibe/ui/screens/EventsAttendingScreen.kt
@@ -4,26 +4,41 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.vibe.data.EventAttending
-import com.example.vibe.model.Event
 import com.example.vibe.ui.viewmodel.EventsUiState
-import com.example.vibe.ui.viewmodel.EventsViewModel
 import com.example.vibe.utils.generateQRCode
 import kotlinx.coroutines.launch
+
+
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -54,8 +69,8 @@ fun EventsAttendingScreen(
                 }
             } else {
                 LazyColumn(
-                    modifier = Modifier.fillMaxSize().padding(top = 68.dp),
-                    contentPadding = PaddingValues(16.dp)
+                    modifier = Modifier.fillMaxSize().padding(top = 104.dp),
+                    contentPadding = PaddingValues(24.dp)
                 ) {
                     items(events) { event ->
                         Log.d("UI", "🔹 Rendering event: ${event.partyname}") // ✅ Log each event
@@ -71,7 +86,7 @@ fun EventsAttendingScreen(
                                 }
                             }
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
+                        Spacer(modifier = Modifier.height(16.dp))
                     }
                 }
             }
