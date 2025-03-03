3bc0b4911f82db11eabb2863eeac1ff843b36aa6
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2025-03-03 13:08:28 -0500

    cleaning up error warnings

--- app/src/main/java/com/example/vibe/network/RSVPApi.kt
+++ app/src/main/java/com/example/vibe/network/RSVPApi.kt
@@ -4,11 +4,8 @@ import com.example.vibe.data.RSVPRequest
 import com.example.vibe.data.RSVPResponse
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface RSVPApi {
 
