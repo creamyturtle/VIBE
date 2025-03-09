23ebc60d03fd9569b3d24f2d31ae3dfc0a00615c
creamyturtle <81478781+creamyturtle@users.noreply.github.com>
2025-03-09 12:16:54 -0500

    translations

--- app/src/main/java/com/example/vibe/ui/screens/PrivacyPolicyScreen.kt
+++ app/src/main/java/com/example/vibe/ui/screens/PrivacyPolicyScreen.kt
@@ -97,43 +97,43 @@ fun PrivacyPolicyScreen(
 
         ContentCard(
             title = stringResource(R.string._3_data_security),
-            content = "Your personal data is stored on our server using 256 bit encryption. Thus even our site admins do not have access to your personal information. Authentication is handled using tokens so as to never transfer your raw user information. All connections are made via an https secure sockets layer certificate. We take every reasonable precaution to protect your data. However, no method of transmission over the Internet or method of electronic storage is 100% secure, and we cannot guarantee absolute security.\n" +
+            content = stringResource(R.string.your_personal_data_is_stored) +
                     "\n" +
-                    "The VIBE app may ask you for permission to access your photos or videos, for the explicit purpose of uploading them to create an event hosted by you. This is a one time permission, and the app does not maintain access to your photos or videos. These are optional features, and you may participate without granting access to this data on your phone.\n" +
+                    stringResource(R.string.the_vibe_app_may_ask) +
                     "\n" +
-                    "The VIBE app may ask you for permission to access your camera, for the purpose of scanning QR codes for users to enter an event. This can be a one time permission, and the app does not maintain access to your camera while not in use. This is an optional feature, only needed if you wish to host events yourself."
+                    stringResource(R.string.the_vibe_app_may_ask_you_for)
         )
 
         ContentCard(
             title = stringResource(R.string._4_data_retention),
-            content = "We retain your personal information as long as necessary to fulfill the purposes outlined in this Privacy Policy, or as required by law. If you cancel your account, your user information will be deleted."
+            content = stringResource(R.string.we_retain_your_personal_information)
         )
 
         ContentCard(
             title = stringResource(R.string._5_sharing_your_information),
-            content = "We do not sell, trade, or otherwise transfer your personal information to outside parties, except:\n" +
+            content = stringResource(R.string.we_do_not_sell_trade) +
                     "\n" +
                     "\n" +
-                    "If required by law or to comply with legal processes.\n" +
+                    stringResource(R.string.if_required_by_law) +
                     "\n" +
-                    "To enforce our Terms & Conditions.\n" +
+                    stringResource(R.string.to_enforce_our_terms_conditions) +
                     "\n" +
-                    "To protect the rights, safety, and security of our users or the public."
+                    stringResource(R.string.to_protect_the_rights)
         )
 
         ContentCard(
             title = stringResource(R.string._6_third_party_services),
-            content = "Our Service may include links to third-party websites or services, such as social media platforms. We are not responsible for the privacy practices or content of these third parties."
+            content = stringResource(R.string.our_service_may_include)
         )
 
         ContentCard(
             title = stringResource(R.string._7_your_rights),
-            content = "Depending on your jurisdiction, you may have rights regarding your personal information, including the right to access, update, or delete it. Please contact us if you have any requests regarding your information."
+            content = stringResource(R.string.depending_on_your_jurisdiction)
         )
 
         ContentCard(
             title = stringResource(R.string._8_changes_to_this_privacy_policy),
-            content = "We may update our Privacy Policy from time to time. We will notify you of any significant changes by posting the new Privacy Policy on this page. Your continued use of our Service constitutes your acceptance of any changes."
+            content = stringResource(R.string.we_may_update)
         )
 
 
